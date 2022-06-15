b2db4449173ddc9b9b09ae1f7d15a4c91dcbaece
FrancoAguirre644 <francoaguirre644@gmail.com>
2022-06-15 01:15:21 -0300

    Users page - add a list of users with their name, character avatar and online status badge.

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -1,6 +1,30 @@
 <template>
-  <q-page class="flex">
-    <h5>Users page!</h5>
+  <q-page class="flex q-pa-md">
+    <q-list class="full-width" separator>
+      <q-item
+        v-for="user in users"
+        :key="user.id"
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0) }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge :color="user.online ? 'light-green-5' : 'grey-4'">
+            {{ user.online ? 'Online' : 'Offline' }}
+          </q-badge>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
@@ -9,5 +33,31 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "UsersPage",
+  setup() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Ruddy Jedrzej",
+          online: true
+        },
+        {
+          id: 2,
+          name: "Mallorie Alessandrini",
+          online: false
+        },
+        {
+          id: 3,
+          name: "Elisabetta Wicklen",
+          online: true
+        },
+        {
+          id: 4,
+          name: "Seka Fawdrey",
+          online: true
+        },
+      ],
+    };
+  },
 });
 </script>
